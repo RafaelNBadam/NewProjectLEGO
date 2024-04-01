1b63374f35790ba8c0c29bb540c24a0a6c4237c5
RafaelNasci <rafanascimento2003@hotmail.com>
2024-04-01 14:05:51 -0300

    Feat: backend logic for adding users in the MongoDB created. feat: Header created

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -1,1 +1,18 @@
-// AQUI FICA A Lógica para cadastra de fato no banco
+import User from "../model/User.model.js";
+
+const userService = {
+  createUser: async (userDetails) => {
+    try {
+      const user = new User(userDetails);
+
+      const savedUser = await user.save();
+
+      return savedUser;
+    } catch (error) {
+      console.log("Erro ao criar o usuário: ", error);
+      throw new Error("Erro ao criar o usuário");
+    }
+  },
+};
+
+export default userService;
